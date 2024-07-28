f99287bba229440fd3fda76c2b2530d76fa8ba99
stormy <1042653777@qq.com>
2024-07-28 21:21:39 +0800

    fix[csj]: block eliminate finish

--- src/resource/midend/Optimizer/OptimizeBlockEliminate.cpp
+++ src/resource/midend/Optimizer/OptimizeBlockEliminate.cpp
@@ -13,9 +13,18 @@ using namespace std;
 void BlockEliminate(RawFunction *&function) {
     auto &blocks = function->basicblock;
     for(auto block = blocks.begin(); block != blocks.end(); ++block){
+        // for(auto pbb : (*block)->pbbs) {
+        //     cout << "Block " << (*block)->name << " has pre bb " << pbb->name << endl;
+        // }
+        // for(auto fbb : (*block)->fbbs) {
+        //     cout << "Block " << (*block)->name << " has follow bb " << fbb->name << endl;
+        // }
         if(block == blocks.begin()) continue;
-        if((*block)->fbbs.empty() && (*block)->pbbs.empty()) {
+        if((*block)->pbbs.empty()) {
             (*block)->isDeleted = true;
+            for(auto fbb : (*block)->fbbs) {
+                fbb->pbbs.remove(*block);
+            }
         }
     }
 }
